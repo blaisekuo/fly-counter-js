706388c9c17b6fcc834db61e71b720c05ed8bab6
blaise <kuotiong@gmail.com>
2018-06-26 15:54:46 +1000

    new branch macwork

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20180528-082051/???";
+const char *build_timestamp = "2018-05-28T08:20:51Z";
+const char *build_version = "1.0";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20180528-082049/???";
+const char *mg_build_timestamp = "2018-05-28T08:20:49Z";
+const char *mg_build_version = "2018052808";
